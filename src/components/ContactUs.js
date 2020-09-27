20cf0ee7e921375b8f4b6aa41caa812db4f15a79
A Suicidal Cow <aaron.bertagnole@gmail.com>
2020-09-26 23:22:53 -0600

    Contact form for sending email not working will update later

---
+++ src/components/ContactUs.js
@@ -0,0 +1,105 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import { Form, Button, Card } from 'react-bootstrap';
+
+const ContactForm = () => {
+  const [state, setState] = useState({
+    name: '',
+    email: '',
+    subject: '',
+    message: '',
+  });
+
+  const [result, setResult] = useState(null);
+
+  const sendEmail = (event) => {
+    event.preventDefault();
+    axios
+      .post('/send', { ...state })
+      .then((response) => {
+        setResult(response.data);
+        setState({
+          name: '',
+          email: '',
+          subject: '',
+          message: '',
+        });
+      })
+      .catch(() => {
+        setResult({
+          success: false,
+          message: 'Something went wrong. Try again later',
+        });
+      });
+  };
+
+  const onInputChange = (event) => {
+    const { name, value } = event.target;
+
+    setState({
+      ...state,
+      [name]: value,
+    });
+  };
+
+  return (
+    <div>
+      {result && (
+        <p className={`${result.success ? 'success' : 'error'}`}>
+          {result.message}
+        </p>
+      )}
+
+      <form onSubmit={sendEmail}>
+        <Card className='container row-4 w-25 bg-dark text-light mt-5'>
+          <Form.Group className='mt-3' controlId='name'>
+            <Form.Label>Full Name</Form.Label>
+            <Form.Control
+              type='text'
+              name='name'
+              value={state.name}
+              placeholder='Enter your full name'
+              onChange={onInputChange}
+            />
+          </Form.Group>
+          <Form.Group controlId='email'>
+            <Form.Label>Email</Form.Label>
+            <Form.Control
+              type='text'
+              name='email'
+              value={state.email}
+              placeholder='Enter your email'
+              onChange={onInputChange}
+            />
+          </Form.Group>
+          <Form.Group controlId='subject'>
+            <Form.Label>Subject</Form.Label>
+            <Form.Control
+              type='text'
+              name='subject'
+              value={state.subject}
+              placeholder='Enter subject'
+              onChange={onInputChange}
+            />
+          </Form.Group>
+          <Form.Group controlId='subject'>
+            <Form.Label>Message</Form.Label>
+            <Form.Control
+              as='textarea'
+              name='message'
+              value={state.message}
+              rows='3'
+              placeholder='Enter your message'
+              onChange={onInputChange}
+            />
+          </Form.Group>
+          <Button className='mb-3' variant='primary' type='submit'>
+            Submit
+          </Button>
+        </Card>
+      </form>
+    </div>
+  );
+};
+
+export default ContactForm;
